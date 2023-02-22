04a9adf500dfd5f8442f8b337ea80c7af6bd9823
clairelist <claire.list2501@gmail.com>
2023-02-22 12:07:36 -0800

    why is gitignore not working

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 //do imports, make sure everything is wired together first!
 import Comment from './Comment';
 import CreateComment from './CreateComment';
+import Comments from './Comments';
 
 function App(){
     return (
         <div>
             <h1>Hello from APP!</h1>
+            <Comments />
             <Comment />
             <CreateComment />
         </div>
---
+++ src/components/Comments.js
@@ -0,0 +1,33 @@
+import {useState, useEffect, useRef} from 'react';
+
+
+function Comments(){
+    const [comments, setComments] = useState([]);
+    const handleCommentsGet = useRef(()=>{});
+
+    handleCommentsGet.current = () => {
+        //handle fetch of data
+        fetch('../data.json')
+            .then((res)=>{
+                //setComments(res.comments); //?
+                const cx = res.json();
+                setComments(cx);
+            })
+    }
+
+    //on first mount of component, do this.
+    useEffect(()=>{
+        handleCommentsGet.current();
+        console.log(comments);
+    }, [])
+
+    
+    return (
+        <div>
+            {/* map over all the comments, and render them here. Order by highest rating first. */}
+
+        </div>
+    )
+}
+
+export default Comments;
